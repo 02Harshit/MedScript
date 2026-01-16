f4d371937930e33e6b27faacda759e529ceb5a85
02Harshit <mittalharshit2004@gmail.com>
2026-01-16 15:02:30 +0530

    Removed puppeteer core and added puppeteer due to bug in production

--- server/utils/pdf.ts
+++ server/utils/pdf.ts
@@ -1,7 +1,11 @@
 import puppeteer from "puppeteer";
 
 export async function htmlToPDF(html: string): Promise<Buffer> {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: true,
+  });
+  
   const page = await browser.newPage();
 
   await page.setContent(html, { waitUntil: "networkidle0" });
