e5ca346fea1353591f29b8009001fc69b792b4f7
02Harshit <mittalharshit2004@gmail.com>
2026-01-16 16:19:38 +0530

    Updated pdf.ts for finding executable path

--- server/utils/pdf.ts
+++ server/utils/pdf.ts
@@ -1,9 +1,11 @@
 import puppeteer from "puppeteer";
 
 export async function htmlToPDF(html: string): Promise<Buffer> {
+  const executablePath = puppeteer.executablePath();
+  console.log("Using chromium at:", executablePath);
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || "/opt/render/.cache/puppeteer/chrome/linux-143.0.7499.192/chrome-linux64/chrome",
+    executablePath,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
