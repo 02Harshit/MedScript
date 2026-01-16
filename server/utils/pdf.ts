4f8d47b5adfd3c47fe8973b9070225a88d7aca69
02Harshit <mittalharshit2004@gmail.com>
2026-01-16 15:47:22 +0530

    Update pdf.ts

--- server/utils/pdf.ts
+++ server/utils/pdf.ts
@@ -2,10 +2,11 @@ import puppeteer from "puppeteer";
 
 export async function htmlToPDF(html: string): Promise<Buffer> {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
     headless: true,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || "/opt/render/.cache/puppeteer/chrome/linux-143.0.7499.192/chrome-linux64/chrome",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
-  
+
   const page = await browser.newPage();
 
   await page.setContent(html, { waitUntil: "networkidle0" });
