fab314efc145bbcb67084885d9b32dd37468031d
02Harshit <mittalharshit2004@gmail.com>
2026-01-16 16:34:01 +0530

    changed the puppeteer with pupeteer core again but with

--- server/utils/pdf.ts
+++ server/utils/pdf.ts
@@ -1,23 +1,45 @@
-import puppeteer from "puppeteer";
+// import puppeteer from "puppeteer";
+
+// export async function htmlToPDF(html: string): Promise<Buffer> {
+//   const executablePath = puppeteer.executablePath();
+//   console.log("Using chromium at:", executablePath);
+//   const browser = await puppeteer.launch({
+//     headless: true,
+//     executablePath,
+//     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+//   });
+
+//   const page = await browser.newPage();
+
+//   await page.setContent(html, { waitUntil: "networkidle0" });
+
+//   const pdfUnit8 = await page.pdf({
+//     format: "A4",
+//     printBackground: true,
+//   });
+
+//   await browser.close();
+//   return Buffer.from(pdfUnit8);
+// }
+
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 
 export async function htmlToPDF(html: string): Promise<Buffer> {
-  const executablePath = puppeteer.executablePath();
-  console.log("Using chromium at:", executablePath);
   const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath(),
     headless: true,
-    executablePath,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
-
   await page.setContent(html, { waitUntil: "networkidle0" });
 
-  const pdfUnit8 = await page.pdf({
+  const pdfUint8 = await page.pdf({
     format: "A4",
     printBackground: true,
   });
 
   await browser.close();
-  return Buffer.from(pdfUnit8);
+  return Buffer.from(pdfUint8);
 }
